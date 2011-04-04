80669f37a608c0abd626062f8d5825d7aeb93283
Raúl Marín <Algunenano@gmail.com>
2011-04-04 19:17:30 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Algunenano
+ *
+ * Created on April 4, 2011, 7:07 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
