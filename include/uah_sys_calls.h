7a06ed3440d70c6f2fb4c61bea61802eaed52fc0
Raúl Marín <Algunenano@gmail.com>
2011-04-22 03:13:06 +0200

    Acceso desde C de las llamadas

--- include/uah_sys_calls.h
+++ include/uah_sys_calls.h
@@ -14,6 +14,24 @@ extern "C" {
 
     void uah_init_traps (void);
 
+#define _UAH_API_sys_call0(type,name)\
+type name(void){\
+    type auxRET;\
+    UAH_REG_D0=_NR_##name;\
+    _EMU_TRAP(0);\
+    auxRET=(type)UAH_REG_D0;\
+    return (type)auxRET;\
+}
+
+#define _UAH_API_sys_call1(type,name,P1type,P1name)\
+type name(P1type,P1name){\
+    type auxRET;\
+    UAH_REG_D0=_NR_##name;\
+    UAH_REG_D1=(long)P1name;\
+    _EMU_TRAP(0);\
+    auxRET=(type)UAH_REG_D0;\
+    return (type)auxRET;\
+}
 
 
 #ifdef	__cplusplus
